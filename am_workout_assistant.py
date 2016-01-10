f5753c3b5c025c71d8cac27a191cfda74044d816
Maslennikov, Andrey <andrew.maslennikov@gmail.com>
2016-01-10 10:41:05 +0300

    Added main program

---
+++ am_workout_assistant.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+from telegram import Telegram
+from message_checker import MessageChecker
+from data_updater import DataUpdater
+
+# init telegram bot
+bot = Telegram()
+ok = bot.init()
+if not ok:
+    print("ERROR (bot init): {}".format(bot.get_messages()))
+    exit(1)
+
+# init message checker
+msg_checker = MessageChecker()
+
+# init data updater
+du = DataUpdater()
+ok = du.init()
+if not ok:
+    print("ERROR (data updater init): {}".format(du.get_error_message()))
+    exit(1)
+
+for message in bot.get_messages():
+    print("Handling message '{}'...".format(message.get_text()))
+    msg_checker.set_message(message.get_text())
+    ok = msg_checker.check()
+    if not ok:
+        bot.send_reply(msg_checker.get_error_message())
+        continue
+
+    ok = du.add_value(msg_checker.get_num_catch_ups())
+    if ok == False:
+        bot.send_reply(du.get_error_message())
+        continue
+
+    # success!
+    bot.send_reply("Successfully added {}!".format(msg_checker.get_num_catch_ups()))
